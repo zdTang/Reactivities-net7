6d95d7812a4120f9ccebc3c681cc74d9a3383583
Neil <trycatchlearn@outlook.com>
2022-12-03 07:44:15 +0700

    End of section 18

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -93,7 +93,8 @@ const Profiles = {
         })
     },
     setMainPhoto: (id: string) => axios.post(`/photos/${id}/setMain`, {}),
-    deletePhoto: (id: string) => axios.delete(`/photos/${id}`)
+    deletePhoto: (id: string) => axios.delete(`/photos/${id}`),
+    updateProfile: (profile: Partial<Profile>) => requests.put(`/profiles`, profile)
 }
 
 const agent = {
--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -62,4 +62,8 @@ export default class UserStore {
     setUserPhoto = (url: string) => {
         if (this.user) this.user.image = url;
     }
+
+    setDisplayName = (name: string) => {
+        if (this.user) this.user.displayName = name;
+    }
 }
