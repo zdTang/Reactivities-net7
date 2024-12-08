38a9aee4d0d6e40f3e8852eb6bac5bf2f3cfdeb9
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-12-08 12:10:53 -0500

    update to Net9

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -7,6 +7,7 @@ using Infrastructure.Photos;
 using Infrastructure.Security;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Persistence;
 
 namespace API.Extensions
@@ -19,42 +20,45 @@ namespace API.Extensions
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
-            services.AddDbContext<DataContext>(options =>
-            {
-                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            //services.AddDbContext<DataContext>(options =>
+            //{
+            //    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-                string connStr;
+            //    string connStr;
 
-                // Depending on if in development or production, use either FlyIO
-                // connection string, or development connection string from env var.
-                if (env == "Development")
-                {
-                    // Use connection string from file.
-                    connStr = config.GetConnectionString("DefaultConnection");
-                }
-                else
-                {
-                    // Use connection string provided at runtime by Flyio.
-                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            //    // Depending on if in development or production, use either FlyIO
+            //    // connection string, or development connection string from env var.
+            //    if (env == "Development")
+            //    {
+            //        // Use connection string from file.
+            //        connStr = config.GetConnectionString("DefaultConnection");
+            //    }
+            //    else
+            //    {
+            //        // Use connection string provided at runtime by Flyio.
+            //        var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-                    // Parse connection URL to connection string for Npgsql
-                    connUrl = connUrl.Replace("postgres://", string.Empty);
-                    var pgUserPass = connUrl.Split("@")[0];
-                    var pgHostPortDb = connUrl.Split("@")[1];
-                    var pgHostPort = pgHostPortDb.Split("/")[0];
-                    var pgDb = pgHostPortDb.Split("/")[1];
-                    var pgUser = pgUserPass.Split(":")[0];
-                    var pgPass = pgUserPass.Split(":")[1];
-                    var pgHost = pgHostPort.Split(":")[0];
-                    var pgPort = pgHostPort.Split(":")[1];
+            //        // Parse connection URL to connection string for Npgsql
+            //        connUrl = connUrl.Replace("postgres://", string.Empty);
+            //        var pgUserPass = connUrl.Split("@")[0];
+            //        var pgHostPortDb = connUrl.Split("@")[1];
+            //        var pgHostPort = pgHostPortDb.Split("/")[0];
+            //        var pgDb = pgHostPortDb.Split("/")[1];
+            //        var pgUser = pgUserPass.Split(":")[0];
+            //        var pgPass = pgUserPass.Split(":")[1];
+            //        var pgHost = pgHostPort.Split(":")[0];
+            //        var pgPort = pgHostPort.Split(":")[1];
 
-                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
-                }
+            //        connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+            //    }
 
-                // Whether the connection string came from the local development configuration file
-                // or from the environment variable from FlyIO, use it to set up your DbContext.
-                options.UseNpgsql(connStr);
-            });
+            //    // Whether the connection string came from the local development configuration file
+            //    // or from the environment variable from FlyIO, use it to set up your DbContext.
+            //    options.UseNpgsql(connStr);
+            //});
+            // Add DbContext
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(
+                config.GetConnectionString("DefaultConnection")));
             services.AddCors(opt =>
             {
                 opt.AddPolicy("CorsPolicy", policy =>
@@ -66,7 +70,11 @@ namespace API.Extensions
                         .WithOrigins("http://localhost:3000");
                 });
             });
-            services.AddMediatR(typeof(List.Handler));
+            //services.AddMediatR(typeof(List.Handler));
+            // Here reference the AddMediatR(MediatRServiceConfiguration)...see document!
+            var newObject = new MediatRServiceConfiguration();
+            var configuration = newObject.RegisterServicesFromAssemblyContaining(typeof(List.Handler));
+            services.AddMediatR(configuration);
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
             services.AddFluentValidationAutoValidation();
             services.AddValidatorsFromAssemblyContaining<Create>();
